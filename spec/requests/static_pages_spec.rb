3e356af6fff3da2b67ebedfe4962018cb90aec12
MarioVitt <vitt.vitt@gmail.com>
2013-10-26 17:27:12 +0200

    Finish layout and routes

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -2,48 +2,31 @@ require 'spec_helper'
 
 describe "Static pages" do
 
-
+	subject { page }
 
 	describe "Home page" do
  
+ 		before { visit root_path }
+          
+		it { should have_selector('h1', text: 'Sample App') }		
 
-		it "should have the h1 'Sample App'" do
-		visit root_path
-		page.should have_selector('h1', :text => 'Sample App')
-		end
+		it { should have_selector('title', text: full_title('')) }
 
-		it "should have the base title" do
-		visit root_path
-		page.should have_selector('title',
-		:text => "Ruby on Rails Tutorial Sample App")
-		end
+		it { should_not have_selector('title', text: '| Home') }
 
-		it "should not have a custom page title" do
-		visit root_path
-		page.should_not have_selector('title', :text => '| Home')
-		end
 
 
 	end
 
 	describe "Help page" do
 
+		before { visit help_path }
+	
+	      it { should have_selector('h1', text: 'Help') }		
 
-		it "should have the h1 'Help'" do
-		visit help_path
-		page.should have_selector('h1', :text => 'Help')
-		end
+		it { should have_selector('title', text: full_title('')) }
 
-		it "should have the base title" do
-		visit help_path
-		page.should have_selector('title',
-		:text => "Ruby on Rails Tutorial Sample App")
-		end
-
-		it "should not have a custom page title" do
-		visit help_path
-		page.should_not have_selector('title', :text => '| Help')
-		end
+		it { should_not have_selector('title', text: '| Help') }
 
 
 
@@ -52,41 +35,27 @@ describe "Static pages" do
 
 	describe "About page" do
     
-		it "should have the h1 'About Us'" do
-		visit about_path
-		page.should have_selector('h1', :text => 'About Us')
-		end
+		before { visit about_path }		
 
-		it "should have the base title" do
-		visit about_path
-		page.should have_selector('title',
-		:text => "Ruby on Rails Tutorial Sample App")
-		end
+	
+		it { should have_selector('h1', text: 'About Us') }		
 
-		it "should not have a custom page title" do
-		visit about_path
-		page.should_not have_selector('title', :text => '| About Us')
-		end
+		it { should have_selector('title', text: full_title('')) }
+
+		it { should_not have_selector('title', text: '| About Us') }
 
   	end
 
 	describe "Contact" do
    
-		it "should have the h1 'Contact'" do
-		visit contact_path
-		page.should have_selector('h1', :text => 'Contact')
-		end
+		before { visit contact_path }
 
-		it "should have the base title" do
-		visit contact_path
-		page.should have_selector('title',
-		:text => "Ruby on Rails Tutorial Sample App")
-		end
+	
+		it { should have_selector('h1', text: 'Contact') }		
 
-		it "should not have a custom page title" do
-		visit contact_path
-		page.should_not have_selector('title', :text => '| Contact')
-		end
+		it { should have_selector('title', text: full_title('')) }
+
+		it { should_not have_selector('title', text: '| Contact') }
 
   	end
 
